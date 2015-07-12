74bae25860abe964ef1cb5d1c03aecbdedb60a7f
wdg8106 <1499728038@qq.com>
2015-07-13 07:12:05 +0800

    fifth

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import tornado.web
  
 from tornado.options import define, options
 define("port", default=8000, help="run on the given port", type=int)
+# database = tornado.Connection('localhost','yinxin',user= 'root',password = '123456')
  
 class IndexHandler(tornado.web.RequestHandler):
     def get(self):
@@ -36,6 +37,31 @@ class ThirdHandler(tornado.web.RequestHandler):
 class ZhuangxiuHandler(tornado.web.RequestHandler):
     def get(self):
         self.render('zhuangxiu.html')
+
+class ApplyHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render('apply.html')
+    def post(self):
+        product = self.get_argument('product')
+        name = self.get_argument('name')
+        phone = self.get_argument('phone')
+        print(type(product),type(name),type(phone))
+        if isinstance(product,unicode):
+            product = product.encode('utf-8')
+        if isinstance(name,unicode):
+            name = name.encode('utf-8')
+        if isinstance(phone,unicode):
+            phone = phone.encode('utf-8')
+        # database.execute('insert into apply()')
+        history = "%s\t%s\t%s\n" %(name,phone,product)
+        f = open('apply.txt','a')
+        f.write(history)
+        f.close()
+        return self.render('success.html')
+
+
+
+        
 
 
  
@@ -68,6 +94,7 @@ if __name__ == '__main__':
             (r'/zhuangxiu',ZhuangxiuHandler,{},'zhuangxiu'),
             (r'/cantact',CantactHandler,{},'cantact'),
             (r'/us',UsHandler,{},'us'),
+            (r'/apply',ApplyHandler,{},'apply'),
             ],
         template_path=os.path.join(os.path.dirname(__file__), "templates"),
         static_path=os.path.join(os.path.dirname(__file__), "static"),
